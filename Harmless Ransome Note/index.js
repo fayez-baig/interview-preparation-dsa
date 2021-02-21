bbada08196df3baf300a8ab9f660221e997264cc
fayez-baig <coderninja16@gmail.com>
2021-02-21 20:06:14 +0530

    harmlessRansomNote Updated

--- Harmless Ransome Note/index.js
+++ Harmless Ransome Note/index.js
@@ -3,13 +3,13 @@
 // For our non programmers, let’s say you have a magazine and you want to create a note text with every word that
 // is present in this specific magazine text, but you’re only allowed to use the words you have been given
 
-function harmlessRansomeNote(noteText, magzineText) {
+function harmlessRansomNote(noteText, magzineText) {
   const noteArr = noteText.split(" ");
   const magzineArr = magzineText.split(" ");
   const magazineObj = {};
   let noteIsPossible = true;
 
-  magazineObj.forEach((word) => {
+  magzineArr.forEach((word) => {
     if (!magazineObj[word]) magazineObj[word] = 0;
     magazineObj[word]++;
   });
@@ -22,6 +22,13 @@ function harmlessRansomeNote(noteText, magzineText) {
 
   return noteIsPossible;
 }
+
+console.log(
+  harmlessRansomNote(
+    "this is a secret note for you from a secret admirer",
+    "puerto rico is a place of great wonder and excitement it has many secret waterfall locations that i am an admirer of you must hike quite a distance to find the secret places as they are far from populated areas but it is worth the effort a tip i have for you is to go early in the morning when it is not so hot out also note that you must wear hiking boots this is one of the best places i have ever visited"
+  )
+);
 
 //Big O notation of the above program has O(n)  (Linear time complexity)
 // OR
