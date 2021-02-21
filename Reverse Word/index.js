0cd12b6458e283ea1a3e202d69f5e603077c59a1
fayez-baig <coderninja16@gmail.com>
2021-02-21 23:12:26 +0530

    enhanced reverse of a string

--- Reverse Word/index.js
+++ Reverse Word/index.js
@@ -1,4 +1,29 @@
-const reverseString = (string) =>
-  string.toLowerCase().split("").reverse().join("");
+// const reverseString = (string) =>
+//   string.toLowerCase().split("").reverse().join("");
 
-console.log(reverseString("Test"));
+//OR
+
+//Reverse a string with like this
+// input:- Coding JavaScript
+//output:- gnidoC tpircSavaJ
+
+const reverseString = (string) => {
+  let newStringArr = [];
+  let stringArr = string.split(" ");
+
+  stringArr.forEach((word) => {
+    let reverseWord = "";
+    for (let i = word.length - 1; i >= 0; i--) {
+      reverseWord += word[i];
+    }
+    newStringArr.push(reverseWord);
+  });
+
+  return newStringArr.join(" ");
+};
+
+console.log(reverseString("Coding JavaScript"));
+
+//Big O notation of the above program has O(n)  (Linear time complexity)
+// OR
+//Big O notation of the above program is O(n) + O(m) = O(m+n)
