9d5854975521be94ae3974a0485cc4ed9c6d0ddc
fayez-baig <coderninja16@gmail.com>
2021-02-25 18:16:25 +0530

    Chunky Monkey More Solutions

--- FreeCodeCamp/Chunky Monkey/index.js
+++ FreeCodeCamp/Chunky Monkey/index.js
@@ -27,3 +27,32 @@ function chunkArrayInGroups(arr, size) {
   return newArr;
 }
 chunkArrayInGroups(["a", "b", "c", "d"], 2);
+
+//OR
+
+function chunkArrayInGroups(arr, size) {
+  var newArr = [];
+  while (arr.length) {
+    newArr.push(arr.splice(0, size));
+  }
+  return newArr;
+}
+
+//OR
+
+function chunkArrayInGroups(arr, size) {
+  var temp = [];
+  var result = [];
+
+  for (var a = 0; a < arr.length; a++) {
+    if (a % size !== size - 1) temp.push(arr[a]);
+    else {
+      temp.push(arr[a]);
+      result.push(temp);
+      temp = [];
+    }
+  }
+
+  if (temp.length !== 0) result.push(temp);
+  return result;
+}
