5277fc5a431492b0511042e573863d6c1971e46b
fayez-baig <coderninja16@gmail.com>
2021-02-25 17:47:25 +0530

    added description

--- FreeCodeCamp/Falsy Bouncer/index.js
+++ FreeCodeCamp/Falsy Bouncer/index.js
@@ -1,3 +1,11 @@
+/*
+Remove all falsy values from an array.
+
+Falsy values in JavaScript are false, null, 0, "", undefined, and NaN.
+
+Hint: Try converting each value to a Boolean.
+*/
+
 function bouncer(arr) {
   const newArr = [];
   for (let i = 0; i < arr.length; i++) {
