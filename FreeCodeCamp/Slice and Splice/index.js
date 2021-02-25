9042b79cf5353e86c9defd4d5123ff69e8df00d0
fayez-baig <coderninja16@gmail.com>
2021-02-25 13:14:06 +0530

    Slice and Splice

---
+++ FreeCodeCamp/Slice and Splice/index.js
@@ -0,0 +1,12 @@
+function frankenSplice(arr1, arr2, n) {
+  const newArray = arr2.slice();
+
+  for (let i = 0; i < arr1.length; i++) {
+    newArray.splice(n, 0, arr1[i]);
+    n++;
+  }
+
+  return newArray;
+}
+
+frankenSplice([1, 2, 3], [4, 5, 6], 1);
