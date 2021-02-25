144126978e6d8effb66236dcbde3bbbea20a33ec
fayez-baig <coderninja16@gmail.com>
2021-02-25 10:28:40 +0530

    Confirm The Ending

---
+++ FreeCodeCamp/Confirm The Ending/index.js
@@ -0,0 +1,7 @@
+function confirmEnding(str, target) {
+  if (str.substring(str.length - target.length, str.length) === target) {
+    return true;
+  } else return false;
+}
+
+confirmEnding("Congratulation", "on");
