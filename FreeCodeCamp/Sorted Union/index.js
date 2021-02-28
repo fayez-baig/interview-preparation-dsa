df832a755a33ba18f4dfccea3a92f449f60b2f45
fayez-baig <coderninja16@gmail.com>
2021-02-28 15:23:56 +0530

    Sorted Union

---
+++ FreeCodeCamp/Sorted Union/index.js
@@ -0,0 +1,32 @@
+/**
+ * Intermediate Algorithm Scripting
+Sorted Union
+Write a function that takes two or more arrays and returns a new array of unique values in the order of the original provided arrays.
+
+In other words, all values present from all arrays should be included in their original order, but with no duplicates in the final array.
+
+The unique numbers should be sorted by their original order, but the final array should not be sorted in numerical order.
+
+Check the assertion tests for examples.
+
+uniteUnique([1, 3, 2], [5, 2, 1, 4], [2, 1]) should return [1, 3, 2, 5, 4].
+ */
+
+function uniteUnique(...arr) {
+  const newArr = [];
+
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = 0; j < arr[i].length; j++) {
+      const current = arr[i][j];
+      if (!newArr[current]) {
+        newArr.push(current);
+      }
+    }
+  }
+  return newArr;
+}
+// OR
+
+const uniteUnique = (...arr) => [...new Set(arr.flat())];
+
+uniteUnique([1, 3, 2], [5, 2, 1, 4], [2, 1]);
