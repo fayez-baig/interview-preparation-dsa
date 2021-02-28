269fe6f41259f0277b4b4707af7b83fa62792cce
fayez-baig <coderninja16@gmail.com>
2021-02-28 15:51:30 +0530

    Convert HTML Entities

---
+++ FreeCodeCamp/Convert HTML Entities/index.js
@@ -0,0 +1,61 @@
+// Convert the characters &, <, >, " (double quote), and ' (apostrophe), in a string to their corresponding HTML entities.
+
+function convertHTML(str) {
+  const temp = str.split("");
+  for (let i = 0; i < strArr.length; i++) {
+    for (var i = 0; i < temp.length; i++) {
+      switch (temp[i]) {
+        case "<":
+          temp[i] = "&lt;";
+          break;
+        case "&":
+          temp[i] = "&amp;";
+          break;
+        case ">":
+          temp[i] = "&gt;";
+          break;
+        case '"':
+          temp[i] = "&quot;";
+          break;
+        case "'":
+          temp[i] = "&apos;";
+          break;
+      }
+    }
+
+    return temp.join("");
+  }
+}
+
+function convertHTML(str) {
+  // Use Object Lookup to declare as many HTML entities as needed.
+  const htmlEntities = {
+    "&": "&amp;",
+    "<": "&lt;",
+    ">": "&gt;",
+    '"': "&quot;",
+    "'": "&apos;",
+  };
+  // Using a regex, replace characters with it's corresponding html entity
+  return str.replace(/([&<>\"'])/g, (match) => htmlEntities[match]);
+}
+convertHTML('Stuff in "quotation marks"');
+
+function convertHTML(str) {
+  // Use Object Lookup to declare as many HTML entities as needed.
+  const htmlEntities = {
+    "&": "&amp;",
+    "<": "&lt;",
+    ">": "&gt;",
+    '"': "&quot;",
+    "'": "&apos;",
+  };
+  //Use map function to return a filtered str with all entities changed automatically.
+  return str
+    .split("")
+    .map((entity) => htmlEntities[entity] || entity)
+    .join("");
+}
+
+// test here
+convertHTML("Dolce & Gabbana");
