e16eb564e2996bf5b50352fbdcbee62aeb6f6364
fayez-baig <coderninja16@gmail.com>
2021-02-28 12:58:37 +0530

    Search and Replace

---
+++ FreeCodeCamp/Search and Replace/index.js
@@ -0,0 +1,48 @@
+/**
+ * Perform a search and replace on the sentence using the arguments provided and return the new sentence.
+
+First argument is the sentence to perform the search and replace on.
+
+Second argument is the word that you will be replacing (before).
+
+Third argument is what you will be replacing the second argument with (after).
+
+Note
+Preserve the case of the first character in the original word when you are replacing it. 
+For example if you mean to replace the word "Book" with the word "dog", it should be replaced as "Dog"
+ */
+
+function myReplace(str, before, after) {
+  const strArr = str.split(" ");
+
+  strArr.forEach((word, index) => {
+    if (word === before) {
+      before.charAt(0).match(/^[A-Z]/)?.[0]
+        ? (strArr[index] = after.charAt(0).toUpperCase() + after.substring(1))
+        : (strArr[index] = after.toLowerCase());
+    }
+  });
+  return strArr.join(" ");
+}
+
+function myReplace(str, before, after) {
+  // Find index where before is on string
+  var index = str.indexOf(before);
+  // Check to see if the first letter is uppercase or not
+  if (str[index] === str[index].toUpperCase()) {
+    // Change the after word to be capitalized before we use it.
+    after = after.charAt(0).toUpperCase() + after.slice(1);
+  } else {
+    // Change the after word to be uncapitalized before we use it.
+    after = after.charAt(0).toLowerCase() + after.slice(1);
+  }
+  // Now replace the original str with the edited one.
+  str = str.replace(before, after);
+
+  return str;
+}
+
+// test here
+myReplace("A quick brown fox jumped over the lazy dog", "jumped", "leaped");
+
+myReplace("I think we should look up there", "up", "Down");
