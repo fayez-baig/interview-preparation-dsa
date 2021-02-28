c3ca12803b4b2902c6cf70caff857e25b4c2dc88
fayez-baig <coderninja16@gmail.com>
2021-02-28 15:52:56 +0530

    Convert HTML Entities bug fixed

--- FreeCodeCamp/Convert HTML Entities/index.js
+++ FreeCodeCamp/Convert HTML Entities/index.js
@@ -2,29 +2,27 @@
 
 function convertHTML(str) {
   const temp = str.split("");
-  for (let i = 0; i < strArr.length; i++) {
-    for (var i = 0; i < temp.length; i++) {
-      switch (temp[i]) {
-        case "<":
-          temp[i] = "&lt;";
-          break;
-        case "&":
-          temp[i] = "&amp;";
-          break;
-        case ">":
-          temp[i] = "&gt;";
-          break;
-        case '"':
-          temp[i] = "&quot;";
-          break;
-        case "'":
-          temp[i] = "&apos;";
-          break;
-      }
+  for (let i = 0; i < temp.length; i++) {
+    switch (temp[i]) {
+      case "<":
+        temp[i] = "&lt;";
+        break;
+      case "&":
+        temp[i] = "&amp;";
+        break;
+      case ">":
+        temp[i] = "&gt;";
+        break;
+      case '"':
+        temp[i] = "&quot;";
+        break;
+      case "'":
+        temp[i] = "&apos;";
+        break;
     }
-
-    return temp.join("");
   }
+
+  return temp.join("");
 }
 
 function convertHTML(str) {
