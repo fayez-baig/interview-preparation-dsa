a60521da1943fad93e0f53365b53d3f2e4e58cd6
fayez-baig <coderninja16@gmail.com>
2021-03-02 10:35:03 +0530

    suffix description

--- CodeWars/index.js
+++ CodeWars/index.js
@@ -1,3 +1,31 @@
+/**
+ ababaa
+ ababaa
+ 111111 = 6
+
+ababaa
+babaa
+000010 = 1
+
+ababaa
+abaa
+111000 = 3
+
+ababaa
+baa
+001000 = 1
+
+ababaa
+aa
+100000 = 1
+
+ababaa
+a
+100000 = 1
+
+6+1+3+1+1+1 = 13
+ */
+
 function suffix(str) {
   const strArr = str.split("");
   let newArr = strArr.slice(0);
