e1021e01bd1eadbd2efaad52cd21353158867286
fayez-baig <coderninja16@gmail.com>
2021-03-02 12:51:42 +0530

    Sum of Numbers using recursion

--- CodeWars/Sum of Numbers/index.js
+++ CodeWars/Sum of Numbers/index.js
@@ -29,4 +29,10 @@ const GetSum = (a, b) => {
   return ((max - min + 1) * (min + max)) / 2;
 };
 
+function GetSum(a, b) {
+  if (a == b) return a;
+  else if (a < b) return a + GetSum(a + 1, b);
+  else return a + GetSum(a - 1, b);
+}
+
 getSum(0, -1);
