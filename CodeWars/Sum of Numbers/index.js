5f4a866921f3e90ecc7c6779ab18030b13db4bd6
fayez-baig <coderninja16@gmail.com>
2021-03-02 12:50:36 +0530

    Sum of Numbers Solution++

--- CodeWars/Sum of Numbers/index.js
+++ CodeWars/Sum of Numbers/index.js
@@ -23,4 +23,10 @@ function getSum(a, b) {
   return sum;
 }
 
+const GetSum = (a, b) => {
+  let min = Math.min(a, b),
+    max = Math.max(a, b);
+  return ((max - min + 1) * (min + max)) / 2;
+};
+
 getSum(0, -1);
