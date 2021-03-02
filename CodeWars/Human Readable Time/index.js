9da2d9f1300421a4d073d6aca3c28b9ee6edb0f6
fayez-baig <coderninja16@gmail.com>
2021-03-02 21:15:21 +0530

    Human Readable Time

---
+++ CodeWars/Human Readable Time/index.js
@@ -0,0 +1,18 @@
+/**
+Write a function, which takes a non-negative integer (seconds) as input and returns the time in a human-readable format (HH:MM:SS)
+
+HH = hours, padded to 2 digits, range: 00 - 99
+MM = minutes, padded to 2 digits, range: 00 - 59
+SS = seconds, padded to 2 digits, range: 00 - 59
+The maximum time never exceeds 359999 (99:59:59)
+
+You can find some examples in the test fixtures.
+ */
+
+const humanReadable = (seconds) => {
+  const HH = ("0" + parseInt(seconds / (60 * 60))).slice(-2);
+  const MM = ("0" + parseInt((seconds / 60) % 60)).slice(-2);
+  const SS = ("0" + (seconds % 60)).slice(-2);
+  console.log(HH + ":" + MM + ":" + SS);
+  return HH + ":" + MM + ":" + SS;
+};
