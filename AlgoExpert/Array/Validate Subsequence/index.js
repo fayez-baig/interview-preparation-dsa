64849e1d3df5424ef6db97c06138d1fe2b9873e2
fayez-baig <coderninja16@gmail.com>
2021-03-08 16:55:12 +0530

    Validate Subsequence another approach added

--- AlgoExpert/Array/Validate Subsequence/index.js
+++ AlgoExpert/Array/Validate Subsequence/index.js
@@ -25,3 +25,16 @@ function isValidSubsequence(array, sequence) {
 }
 
 //  O(n) time | O(1) space - where n is the length of the array
+
+function isValidSubsequence(array, sequence) {
+  let index = 0;
+  for (let i = 0; i < array.length; i++) {
+    if (index === sequence.length) break;
+    if (array[i] === sequence[index]) {
+      index++;
+    }
+  }
+  return index === sequence.length;
+}
+
+console.log(isValidSubsequence([5, 1, 22, 25, 6, -1, 8, 10], [1, 6, -1, 10]));
