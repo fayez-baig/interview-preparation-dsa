72aeb4ad53524c2aff0e74e22ac9696e9ea8f86a
fayez-baig <coderninja16@gmail.com>
2021-03-08 17:41:06 +0530

    Three Number Sum

---
+++ AlgoExpert/Array/Three Number Sum/index.js
@@ -0,0 +1,24 @@
+function threeSum(array, target) {
+  array.sort((a, b) => a - b);
+  let newArr = [];
+  let currentNumber = array[0];
+  let left = 0;
+  let right = 0;
+  for (let i = 0; i < array.length; i++) {
+    left = i + 1;
+    right = array.length - 1;
+    while (left < right) {
+      let currentSum = currentNumber + array[left] + array[right];
+      if (currentSum === target) {
+        newArr.push([currentNumber, array[left], array[right]]);
+        left++;
+        right--;
+      } else if (currentSum < target) left++;
+      else if (currentSum > target) right--;
+    }
+    currentNumber = array[i + 1];
+  }
+  return newArr;
+}
+
+console.log(threeSum([12, 3, 1, 2, -6, 5, -8, 6], 0));
